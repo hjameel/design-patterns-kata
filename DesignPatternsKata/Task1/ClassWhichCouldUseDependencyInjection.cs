3ee9d51125beef48b362352ab4fcf2f86fec308c
Hanif Jameel <hanif.jameel@btinternet.com>
2016-03-02 23:29:28 +0000

    Add dependency injection exercise.

---
+++ DesignPatternsKata/Task1/ClassWhichCouldUseDependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DesignPatternsKata
+{
+	public static class App
+	{
+		public static void Main()
+		{
+			new Friend().SayHi();
+		}
+	}
+
+	public class Friend
+	{
+		public void SayHi()
+		{
+			// By instantiating an instance of Voice here, we've created tight coupling between
+			// Friend and the FriendsVoice implementation.
+			new FriendsVoice().Say("Hi!");
+		}
+	}
+
+	public class FriendsVoice
+	{
+		public void Say(string words)
+		{
+			Console.WriteLine(words);
+		}
+	}
+}
+
--- Tests/Class1.cs
+++ Tests/Class1.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using DesignPatternsKata;
 
 namespace Tests
 {
-    public class Class1
-    {
-    }
+	[TestFixture]
+	public class Task1_DependencyInjectionPattern
+	{
+		[Test]
+		public void My_friend_should_say_hi()
+		{
+			var friend = new Friend();
+
+			friend.SayHi();
+
+			// An important object oriented design principle, which the Gang of Four recommend is:
+
+			// 	  "Program to an interface, not an implementation"
+
+			// Update the Friend class, so that it adhere's to this principle. This will enable
+			// you to use the mock object below to replace this assertion and make sure that your
+			// friend used their voice and said Hi!
+
+			Assert.Fail("I have no idea if my friend spoke or not");
+
+			// Question: Where should we construct the objects which make up our application, if
+			// not at the point where they are used?
+		}
+
+		class MockVoice
+		{
+			string _words;
+
+			public void Say(string words)
+			{
+				_words = words;
+			}
+
+			public void AssertWasSpoken(string expectedWords)
+			{
+				Assert.That(_words, Is.EqualTo(expectedWords));
+			}
+		}
+	}
 }
