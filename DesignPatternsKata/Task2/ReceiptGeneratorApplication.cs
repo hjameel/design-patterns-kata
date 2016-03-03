a0e43ba02e5a4077bb5164a0ca1c137c5451d48d
Hanif Jameel <hanif.jameel@7digital.com>
2016-03-03 10:08:54 +0000

    Add next task.

---
+++ DesignPatternsKata/Task2/ReceiptGeneratorApplication.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatternsKata.Task2
+{
+	public class ReceiptGenerator
+	{
+		private readonly Country _salesCountry;
+
+		public ReceiptGenerator(Country salesCountry)
+		{
+			_salesCountry = salesCountry;
+		}
+
+		public string GetReceiptForItem()
+		{
+			return "Price: 10 Tax:0 Discount:0";
+		}
+	}
+
+	public enum Country
+	{
+		UnitedKingdom
+	}
+}
---
+++ Tests/Task2_TemplateMethodPattern.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable InconsistentNaming
+using DesignPatternsKata.Task2;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	public class Task2_TemplateMethodPattern
+	{
+		[Test]
+		public void Should_apply_10_percent_tax_rate_in_uk()
+		{
+			var receiptGenerator = new ReceiptGenerator(Country.UnitedKingdom);
+
+			var receipt = receiptGenerator.GetReceiptForItem();
+
+			Assert.That(receipt, Is.EqualTo("Price: 10 Tax:0 Discount:0"));
+		}
+	}
+}
