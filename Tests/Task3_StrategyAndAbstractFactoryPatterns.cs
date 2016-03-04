197ff06e02d3660bb698c73f1a8ce29890775692
Hanif Jameel <hanif.jameel@7digital.com>
2016-03-04 10:45:53 +0000

    Re-order tests in task 3

--- Tests/Task3_StrategyAndAbstractFactoryPatterns.cs
+++ Tests/Task3_StrategyAndAbstractFactoryPatterns.cs
@@ -32,6 +32,34 @@ namespace Tests
 		// you find more useful for thinking about the design of your code?
 
 		[Test]
+		public void Should_apply_20_percent_tax_rate_in_UK()
+		{
+			var receipt = new Receipt(new UnitedKingdomTaxAmountCalculator());
+
+			Assert.That(receipt.ForItemCosting(100), Is.EqualTo("Price: 120 Tax: 20"));
+		}
+
+		[TestCase(10, 11, 1)]
+		[TestCase(11, 12.1, 1.1)]
+		public void Should_apply_10_percent_tax_rate_in_US_for_items_10_dollars_and_over(
+			decimal price, decimal priceAfterTax, decimal taxAmount)
+		{
+			var receipt = new Receipt(new UnitedStatesTaxAmountCalculator());
+
+			Assert.That(
+				receipt.ForItemCosting(price),
+				Is.EqualTo(string.Format("Price: {0} Tax: {1}", priceAfterTax, taxAmount)));
+		}
+
+		[Test]
+		public void Should_apply_0_percent_tax_rate_in_US_for_items_under_10_dollars()
+		{
+			var receipt = new Receipt(new UnitedStatesTaxAmountCalculator());
+
+			Assert.That(receipt.ForItemCosting(9), Is.EqualTo("Price: 9 Tax: 0"));
+		}
+
+		[Test]
 		public void Should_apply_15_percent_tax_rate_in_France()
 		{
 			// Add support for French receipts, with a tax rate of 15 percent.
@@ -61,34 +89,6 @@ namespace Tests
 
 			// Something to think about once you've done this step:
 			//   How many classes would be required to have a custom optional discount for each country?
-		}
-
-		[Test]
-		public void Should_apply_20_percent_tax_rate_in_UK()
-		{
-			var receipt = new Receipt(new UnitedKingdomTaxAmountCalculator());
-
-			Assert.That(receipt.ForItemCosting(100), Is.EqualTo("Price: 120 Tax: 20"));
-		}
-
-		[TestCase(10, 11, 1)]
-		[TestCase(11, 12.1, 1.1)]
-		public void Should_apply_10_percent_tax_rate_in_US_for_items_10_dollars_and_over(
-			decimal price, decimal priceAfterTax, decimal taxAmount)
-		{
-			var receipt = new Receipt(new UnitedStatesTaxAmountCalculator());
-
-			Assert.That(
-				receipt.ForItemCosting(price),
-				Is.EqualTo(string.Format("Price: {0} Tax: {1}", priceAfterTax, taxAmount)));
-		}
-
-		[Test]
-		public void Should_apply_0_percent_tax_rate_in_US_for_items_under_10_dollars()
-		{
-			var receipt = new Receipt(new UnitedStatesTaxAmountCalculator());
-
-			Assert.That(receipt.ForItemCosting(9), Is.EqualTo("Price: 9 Tax: 0"));
 		}
 	}
 }
